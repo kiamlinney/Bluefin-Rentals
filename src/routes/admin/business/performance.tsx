e69695411a188f577d210843534e50f6bd56f6e8
Liam Kinney <kinne351@umn.edu>
2026-06-03 19:19:41 -0500

    Finished admin sidebar

--- src/routes/admin/business/performance.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/admin/business/performance')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/admin/business/performance"!</div>
-}
--- src/routes/admin/business/transaction-history.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/admin/business/transaction-history')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/admin/business/transaction-history"!</div>
-}
